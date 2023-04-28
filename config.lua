e249f11a1e0d954d25cb4441df37a59bc86920c2
Niklas Haas <git@haasn.dev>
2023-04-28 15:55:11 +0200

    Various changes

--- config.lua
+++ config.lua
@@ -1,56 +1,83 @@
---[[
- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
- `lvim` is the global options object
-]]
-
 -- vim options
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.relativenumber = true
+local tabstop = 4
+vim.opt.autochdir = true
+vim.opt.shiftwidth = tabstop
+vim.opt.tabstop = tabstop
+vim.opt.softtabstop = tabstop
+vim.opt.expandtab = true
+vim.opt.relativenumber = false
+vim.opt.mouse = ""
+vim.opt.colorcolumn = "81"
+vim.opt.scrolloff = 5
+vim.opt.backspace = "indent"
+vim.opt.whichwrap = "b,s"
+vim.opt.timeoutlen = 300
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldnestmax = 2
+vim.opt.foldlevel = 0
+vim.opt.foldminlines = 100
+vim.opt.foldopen = "block,hor,mark,percent,quickfix,search,tag,undo"
 
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+    enabled = true,
+    pattern = "*.lua",
+    timeout = 1000,
 }
+
 -- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
+lvim.use_icons = false
 
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode[";"] = ":"
+lvim.keys.normal_mode["<C-s>"] = ":wa<cr>"
+lvim.keys.normal_mode["<C-d>"] = "<cmd>vim.lsp.buf.definition()<CR>"
+lvim.keys.normal_mode["<C-p>"] = lvim.builtin.which_key.mappings["f"][1]
 
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<M-h>"] = "<C-w>h"
+lvim.keys.normal_mode["<M-j>"] = "<C-w>j"
+lvim.keys.normal_mode["<M-k>"] = "<C-w>k"
+lvim.keys.normal_mode["<M-l>"] = "<C-w>l"
+
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["s"] = { "<cmd>wa<cr>", "Save all" }
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["n"] = lvim.builtin.which_key.mappings["h"]
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["u"] = { "<cmd>UndotreeToggle<CR>", "Undo Tree" }
 
--- -- Change theme settings
--- lvim.colorscheme = "lunar"
+-- Change theme settings
+lvim.colorscheme = "vscode"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
+lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.treesitter.rainbow.enable = false
 
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.telescope.pickers = {
+    find_files = {
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+        }
+    },
+}
 
--- -- always installed on startup, useful for parsers without a strict filetype
--- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
+-- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
+lvim.lsp.diagnostics.virtual_text = true
 
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
--- --- disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
+-- always installed on startup, useful for parsers without a strict filetype
+lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
 
 -- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
@@ -94,12 +121,74 @@ lvim.builtin.treesitter.auto_install = true
 -- }
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
--- lvim.plugins = {
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+    "lunarvim/colorschemes",
+    "Mofiqul/vscode.nvim",
+    -- "mrjones2014/nvim-ts-rainbow",
+    "mbbill/undotree",
+
+    -- {
+    --     "folke/trouble.nvim",
+    --     cmd = "TroubleToggle",
+    -- },
+
+    -- {
+    --     "debugloop/telescope-undo.nvim",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "debugloop/telescope-undo.nvim",
+    --     },
+    --     config = function()
+    --         require("telescope").setup({
+    --             extensions = {
+    --                 undo = {
+    --                     side_by_side = true,
+    --                     layout_strategy = "vertical",
+    --                     layout_config = {
+    --                         preview_height = 0.8,
+    --                         width = 0.8,
+    --                         height = 0.8,
+    --                     },
+    --                 },
+    --             },
+    --         })
+    --         require("telescope").load_extension("undo")
+    --         -- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+    --     end,
+    -- },
+
+    -- {
+    --     "ggandor/leap.nvim",
+    --     name = "leap",
+    --     config = function()
+    --         require("leap").add_default_mappings()
+    --     end,
+    -- },
+
+    {
+        'wfxr/minimap.vim',
+        build = "cargo install --locked code-minimap",
+        cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+        config = function()
+            vim.cmd("let g:minimap_width = 10")
+            vim.cmd("let g:minimap_auto_start = 1")
+            vim.cmd("let g:minimap_auto_start_win_enter = 1")
+        end,
+    },
+}
+
+-- GitHub Copilot
+table.insert(lvim.plugins, {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+        vim.defer_fn(function()
+            require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+            require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+        end, 100)
+    end,
+})
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 -- vim.api.nvim_create_autocmd("FileType", {
@@ -109,3 +198,114 @@ lvim.builtin.treesitter.auto_install = true
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+
+-- C/C++ language support
+
+-- Additional Plugins
+table.insert(lvim.plugins, {
+    "p00f/clangd_extensions.nvim",
+})
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+
+-- some settings can only passed as commandline flags, see `clangd --help`
+local clangd_flags = {
+    "--background-index",
+    "--fallback-style=Google",
+    "--all-scopes-completion",
+    "--clang-tidy",
+    "--log=error",
+    "-j=3",
+    -- "--suggest-missing-includes",
+    "--cross-file-rename",
+    "--completion-style=detailed",
+    "--pch-storage=memory",     -- could also be disk
+    "--folding-ranges",
+    "--enable-config",          -- clangd 11+ supports reading from .clangd configuration file
+    "--offset-encoding=utf-16", -- temporary fix for null-ls / copilot
+    -- "--limit-references=1000",
+    -- "--limit-resutls=1000",
+    "--malloc-trim",
+    -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+    "--header-insertion=never",
+    "--header-insertion-decorators=false",
+    -- "--query-driver=<list-of-white-listed-complers>"
+}
+
+local provider = "clangd"
+
+local custom_on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "<leader>lh", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
+    vim.keymap.set("x", "<leader>lA", "<cmd>ClangdAST<cr>", opts)
+    vim.keymap.set("n", "<leader>lH", "<cmd>ClangdTypeHierarchy<cr>", opts)
+    vim.keymap.set("n", "<leader>lt", "<cmd>ClangdSymbolInfo<cr>", opts)
+    vim.keymap.set("n", "<leader>lm", "<cmd>ClangdMemoryUsage<cr>", opts)
+
+    require("clangd_extensions.inlay_hints").setup_autocmd()
+    require("clangd_extensions.inlay_hints").set_inlay_hints()
+end
+
+local status_ok, project_config = pcall(require, "rhel.clangd_wrl")
+if status_ok then
+    clangd_flags = vim.tbl_deep_extend("keep", project_config, clangd_flags)
+end
+
+local custom_on_init = function(client, bufnr)
+    require("lvim.lsp").common_on_init(client, bufnr)
+    require("clangd_extensions.config").setup {}
+    require("clangd_extensions.ast").init()
+    vim.cmd [[
+  command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+  command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+  command ClangdTypeHierarchy lua require('clangd_extensions.type_hierarchy').show_hierarchy()
+  command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+  command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+  ]]
+end
+
+local opts = {
+    cmd = { provider, unpack(clangd_flags) },
+    on_attach = custom_on_attach,
+    on_init = custom_on_init,
+}
+
+-- require("lvim.lsp.manager").setup("clangd", opts)
+
+-- install codelldb with :MasonInstall codelldb
+-- configure nvim-dap (codelldb)
+lvim.builtin.dap.on_config_done = function(dap)
+    dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+            -- provide the absolute path for `codelldb` command if not using the one installed using `mason.nvim`
+            command = "codelldb",
+            args = { "--port", "${port}" },
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+        },
+    }
+
+    dap.configurations.cpp = {
+        {
+            name = "Launch file",
+            type = "codelldb",
+            request = "launch",
+            program = function()
+                local path
+                vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" }, function(input)
+                    path = input
+                end)
+                vim.cmd [[redraw]]
+                return path
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+        },
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+end
